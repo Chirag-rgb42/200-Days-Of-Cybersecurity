5810dd6cf2ef9846c7eed89fb8af2935e5c95ac0
Chirag-rgb42 <chsuthar9649@gmail.com>
2026-07-18 18:44:18 +0530

    Day 4: Build automated port scanner simulation and update logs

--- Day_4/DAY_4.py
+++ Day_4/DAY_4.py
@@ -6,16 +6,14 @@
 print("=== AUTOMATED NETWORK PORT SCANNER ===")
 print("Target IP: 192.168.1.1\n")
 
-# 1. Loop through ports 1 to 30 using a for loop and range()
 # range(1, 31) will loop from number 1 up to number 30
 for port in range(1, 31):
     
-    # 2. Simulate hitting common dangerous ports
-    if port == 21:
+    if port == 13:
         print(f"📡 Port {port}: [OPEN] <-- ⚠️ WARNING: Insecure FTP detected!")
-    elif port == 22:
+    elif port == 20:
         print(f"📡 Port {port}: [OPEN] <-- ✅ Secure SSH Access available.")
-    elif port == 23:
+    elif port == 27:
         print(f"📡 Port {port}: [OPEN] <-- 🚨 CRITICAL: Vulnerable Telnet protocol active!")
     else:
         # 3. For all other ports, show they are closed
